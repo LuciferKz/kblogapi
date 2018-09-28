f4344dc9d4ccbb84791356982b089e91933d8402
Kim Zhang <ZHAN712@sh.arvatochina.com>
2018-09-28 18:30:32 +0800

    fix query

--- routers/article.js
+++ routers/article.js
@@ -63,7 +63,7 @@ router.post('/remove', function (req, res, next) {
 
 router.post('/fetch', function (req, res, next) {
   if (req.body.query) {
-    handlerArticle.fetch(query, function (result) {
+    handlerArticle.fetch(req.body.query, function (result) {
       res.json(result)
     })
   } else {
